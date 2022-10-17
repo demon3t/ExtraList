0529739c251aa579e94c0ce906f75a023a469f74
sadyrin.inc@outlook.com <sadyrin.inc@outlook.com>
2022-10-17 16:35:02 +0300

    delete

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,28 +1,12 @@
 ﻿using System;
-using ExtraList.TimeList;
 
-namespace Test
+namespace test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            TimeList<int> ints = new TimeList<int>(6);
-
-
-
-            ints.Add(0);
-            ints.Add(1);
-            ints.Add(2);
-            ints.Add(3);
-            ints.Add(123, DateTime.Now.AddSeconds(-15));
-            ints.Add(4);
-            ints.Add(5);
-            ints.Add(6);
-
-            ITime time = ints;
-
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
